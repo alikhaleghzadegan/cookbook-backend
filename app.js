88dfa5709414811ece0ed2366857e555aa1c0086
chinch42 <vanessakolund@gmail.com>
2022-07-28 22:35:40 +0200

    crated db

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-const express = require("express");
-const mysql = require("mysql");
-const app = express();
 // const { mysqlmod } = require("./mysqlmod");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  password: "password",
-  database: "databasename",
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL Server!");
-});
-
-let recipe = [
-  { id: 1, name: "ABC" },
-  { id: 2, name: "DEF" },
-];
-
-// app.get("/", (req, res) => {
-//   connection.query("SELECT * from users LIMIT 1", (err, rows) => {
-//     if (err) throw err;
-//     console.log("The data from users table are: \n", rows);
-//     connection.end();
-//   });
+let express = require("express");
+const app = express();
+const path = require("path");
+const { next } = require("process");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const namepswd = process.env.Pswd;
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+const name = process.env.Pswd;
+//   res.status(200).json({ name });
 // });
+const portglobal = 8000;
+const databaseName = "recipes_db";
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: name,
+  // database: "recipes_db",
+});
 
-app.listen(3000, () => {
-  console.log("running in port 3000");
+db.connect((err) => {
+  console.log("Conneting to ", databaseName);
+});
+app.get("/", (req, res) => {
+  res.send(`<h1>Test Server</h1>`);
+});
+
+app.get("/db", (req, res, err) => {
+  let sql = "CREATE DATABASE recipes_db";
+  db.query(sql, (result) => {
+    err ? console.log("Error creating database: ", err) : console.log(result);
+    res.send(`
+      <h1>Database created sucessfully</h1>
+      ${result}
+  SHOW DATABASE TABLE recipes_db
+    
+    `);
+  });
+});
+
+let server = app.listen(portglobal, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log("Server listening at http://%s:%s", host, port);
 });
